4eba0d7431dbd0311670d0f962092561b1e790c8
HunterDietrich <70781742+HunterDietrich@users.noreply.github.com>
2020-10-10 00:29:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import time
+
+print("Welcome to Making Toast")
+print()
+
+
+def start():
+    print("You woke up hungry for a early morning snack and head toward the kitchen to get and get ready to make a meal.")
+    time.sleep(3)
+    print("Man im hungry")
+    time.sleep(3)
+    print("Hmm i think im hungry for...")
+
+    scene1 = input("What are you hungry for? \nA)Toast \nB)Toast \nC)Toast \n")
+
+start() 
