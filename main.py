b004de2c86c7502d78a812e440ec84e1174599fe
HunterDietrich <70781742+HunterDietrich@users.noreply.github.com>
2020-10-17 00:29:36 +0000

    code

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ print("Welcome to Making Toast")
 print()
 
 
+options = ["A", "B", "C"]
+
 def start():
     print("You woke up hungry for a early morning snack and head toward the kitchen to get and get ready to make a meal.")
     time.sleep(3)
@@ -11,6 +13,9 @@ def start():
     time.sleep(3)
     print("Hmm i think im hungry for...")
 
-    scene1 = input("What are you hungry for? \nA)Toast \nB)Toast \nC)Toast \n")
+    scene1 = input("What are you hungry for? \nA)Toast \nB)Toast \nC)Toast \n").upper()
+
+while scene1 not iin options:
+    scene1 = input("Not an Option. Choose  \nA)Toast \nB)Toast \nC)Toast \n")
 
 start() 
